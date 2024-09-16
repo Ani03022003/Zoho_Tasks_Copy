d3f9dd944aeadb60a3678b07969ae5ea4e01d1c4
Ani03022003 <anishuzumaki0403@gmail.com>
2024-09-16 18:45:50 +0530

    First Commit

--- Android Assignments/Assignment4&5-SimpleApp-GuessTheNumber/app/src/main/java/com/example/guessthenumber/screens/game/GameFragment.kt
+++ Android Assignments/Assignment4&5-SimpleApp-GuessTheNumber/app/src/main/java/com/example/guessthenumber/screens/game/GameFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -46,6 +47,20 @@ class GameFragment : Fragment() {
             if(hasFinished){
                 findNavController().navigate(GameFragmentDirections.actionGameFragmentToFinishFragment(viewModel.loseString, viewModel.randomNumber))
                 viewModel.onOver()
+            }
+        })
+
+        viewModel.nonNull.observe(viewLifecycleOwner, Observer { isNull ->
+            if(isNull){
+                Toast.makeText(context, "Please Enter a number", Toast.LENGTH_LONG).show()
+                viewModel.onFinishNonNull()
+            }
+        })
+
+        viewModel.reset.observe(viewLifecycleOwner, Observer {
+            isReset -> if(isReset) {
+                binding.numText.setText("")
+                viewModel.onFinishReset()
             }
         })
 
