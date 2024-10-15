f6f1e9f3a39bb8bcfffcff168bf67506c9542731
Ani03022003 <anishuzumaki0403@gmail.com>
2024-10-15 13:11:29 +0530

    First Commit

--- StringCompateTo.java
+++
@@ -1,9 +0,0 @@
-public class StringCompateTo {
-    public static void main(String[] args) {
-        String str1="A";
-        String str2="B";
-
-        System.out.println(str1.compareTo(str2));
-        System.out.println(str2.compareTo(str1));
-    }
-}
